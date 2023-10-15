c5d0c1246c2ee93b2ecc9c6ea0d7f4ae23ccbf0c
P10R10 <asantiago@keemail.me>
2023-10-15 19:19:17 +0100

    cook

--- src/Cook.java
+++ src/Cook.java
@@ -1,3 +1,26 @@
 public class Cook extends Thread {
+    private int cookID;
+    private int maxBoars;
+    private Table table;
+
+    public Cook(int cookID, int maxBoars, Table table) {
+        super("Cook #" + cookID);
+        this.cookID = cookID;
+        this.maxBoars = maxBoars;
+        this.table = table;
+    }
+
+    @Override
+    public void run() {
+        int i = 0;
+        for (; i < maxBoars; i++) {
+            try {
+                table.serve(new Boar(cookID, i));
+            } catch (InterruptedException e) {
+                System.out.println(getName() + " was interrupted");
+            }
+        }
+        System.out.println(getName() + " has cooked " + i + "boars.");
+    }
 
 }
--- src/Table.java
+++ src/Table.java
@@ -1,2 +1,20 @@
+import java.util.LinkedList;
+
 public class Table {
+    private final int MAX = 10;
+    private final LinkedList<Boar> boars;
+
+    public Table() {
+        boars = new LinkedList<>();
+    }
+
+    private boolean isFull() {
+        return boars.size() == MAX;
+    }
+
+    public synchronized void serve(Boar boar) {
+        if (!isFull()) {
+            boars.add(boar);
+        }
+    }
 }
